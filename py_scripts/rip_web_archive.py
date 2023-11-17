01f3066c3e88239472e1623d2679a900a05beaeb
Jan Szejko <janek37@gmail.com>
2023-11-17 16:35:31 +0100

    Improve rip script

--- py_scripts/rip_web_archive.py
+++ py_scripts/rip_web_archive.py
@@ -1,5 +1,7 @@
 import json
 import logging
+import re
+import sys
 import time
 from typing import NamedTuple, Iterator, Any
 
@@ -13,10 +15,12 @@ NOT_CREATED_TEXT = "This page has not been created.  If you log in, you can crea
 MAINTENANCE_TEXT = "Currently undergoing work - we should be back up by the end of February!"
 
 ARCHIVE_OVERRIDES = {
-    "aabaaaaeca": (
-        "https://web.archive.org/web/20040903185318/http://www.qwantz.com/storyengine/index.asp?pos=aabaaaaeca"
-    ),
+    "aabaaaaeca": "20040903185318",
 }
+INTERNAL_LINK_PATTERN = re.compile(
+    r'href="https?://web.archive.org/web/[0-9]{14}/'
+    r'https?://(?:www.)?qwantz.com(?::80)?/storyengine/index.asp\?pos=(?P<id>[a-e]+)"'
+)
 
 
 class Option(NamedTuple):
@@ -56,29 +60,39 @@ class Node(NamedTuple):
 
 
 def main():
-    logger.setLevel(logging.INFO)
-    for node in iterate_nodes("a"):
+    logging.basicConfig(level=logging.INFO)
+    for node in iterate_nodes():
         print(node.to_dict())
 
 
-def iterate_nodes(start_node: str) -> Iterator[Node]:
-    known_ids = set()
-    stack = [start_node]
-    while stack:
-        node_id = stack.pop()
-        if node_id not in known_ids:
-            logging.info(f"processing node {node_id}")
-            if node := get_node(node_id):
-                yield node
-                known_ids.add(node.node_id)
-                stack.extend(option.node_id for option in node.options)
+def iterate_nodes() -> Iterator[Node]:
+    # https://web.archive.org/web/timemap/json?url=qwantz.com/storyengine/index.asp&matchType=prefix&collapse=urlkey&output=json&fl=original&filter=!statuscode:[45]..&limit=10000
+    archived_urls = json.load(sys.stdin)
+    node_ids = [urls[0][urls[0].index('pos=') + 4:] for urls in archived_urls if 'pos=' in urls[0]]
+    node_id_set = set()
+    for node_id in node_ids:
+        logging.info(f"processing node {node_id}")
+        if node := get_node(node_id):
+            yield node
+            node_id_set.add(node_id)
+
+    for node_id in node_ids:
+        parent_id = node_id[:-1]
+        if len(node_id) > 1 and parent_id not in node_id_set:
+            child_ids = (
+                child_id
+                for child_id in node_ids
+                if len(child_id) == len(node_id) and child_id.startswith(parent_id)
+            )
+            options = [Option(text=f"Option {i}", node_id=child_id) for i, child_id in enumerate(child_ids, start=1)]
+            yield Node(node_id=parent_id, text="Missing node :(", options=options, author="")
+            node_id_set.add(parent_id)
 
 
-def get_node(node_id: str) -> Node:
+def get_node(node_id: str) -> Node | None:
     html = get_html(node_id)
-    if not html or NOT_CREATED_TEXT in html or MAINTENANCE_TEXT in html:
-        text = "#not_found" if not html else "#not_created" if NOT_CREATED_TEXT in html else "#maintenance"
-        return Node(node_id=node_id, text=text, options=[], author="")
+    if NOT_CREATED_TEXT in html or MAINTENANCE_TEXT in html:
+        return None
     soup = BeautifulSoup(html, features="html.parser")
     if node_id == "a":
         text = "Choose a story!"
@@ -94,6 +108,7 @@ def get_node(node_id: str) -> Node:
             assert False  # does not happen
         if text.endswith("<p>"):
             text = text[:-len("<p>")].strip()
+        text = INTERNAL_LINK_PATTERN.sub(r'href="/\g<id>"', text)
         author = font_elements[-2].text if len(font_elements) >= 6 else "unknown"
     if soup.ul:
         options = [Option(text=link.text, node_id=link.attrs["href"].split("=")[-1]) for link in soup.ul.find_all("a")]
@@ -102,20 +117,16 @@ def get_node(node_id: str) -> Node:
     return Node(node_id=node_id, text=text, options=options, author=author)
 
 
-def get_html(node_id) -> str | None:
-    if node_id in ARCHIVE_OVERRIDES:
-        archived_url = ARCHIVE_OVERRIDES[node_id]
-    else:
-        web_archive_url = f'https://archive.org/wayback/available?url=qwantz.com/storyengine/index.asp?pos={node_id}'
-        response = get_response(web_archive_url).json()
-        archived_url = response["archived_snapshots"]["closest"]["url"] if response["archived_snapshots"] else None
-        if not archived_url:
-            logger.info(response)
-    if archived_url:
-        while True:
-            html = get_response(archived_url).text
-            if html:
-                return html
+def get_html(node_id) -> str:
+    timestamp = ARCHIVE_OVERRIDES.get(node_id, "20050201000000")
+    archived_url = f'https://web.archive.org/web/{timestamp}/http://www.qwantz.com/storyengine/index.asp?pos={node_id}'
+    while True:
+        response = get_response(archived_url)
+        if response.status_code == 404:
+            assert False, f'Node {node_id} not found'
+        else:
+            if response.text:
+                return response.text
             logger.info(f"empty response from url {archived_url}, retrying")
 
 
@@ -126,6 +137,7 @@ def get_response(url: str) -> Response:
             return requests.get(url, headers={"User-Agent": "Story Engine Bot, contact: janek37@gmail.com"})
         except RequestException:
             logger.info(f"error from url {url}, retrying...")
+            time.sleep(4)
 
 
 if __name__ == '__main__':
