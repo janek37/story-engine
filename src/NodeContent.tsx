2910108931d8e61d42f4b21b0c203f8a33d2f7e2
Jan Szejko <janek37@gmail.com>
2026-09-16 22:55:27 +0200

    Fix skull

--- src/NodeContent.tsx
+++ src/NodeContent.tsx
@@ -5,7 +5,7 @@ function NodeContent({gameNode}: {gameNode: GameNode}): React.ReactElement {
   return <>
     <p className={'author'}>{gameNode.author}</p>
     <div dangerouslySetInnerHTML={{__html: gameNode.text}}/>
-    {!gameNode.choices.length && <><p>The End</p><p><img src={"/skull.gif"} alt={"human skull"} height={50} /></p></>}
+    {!gameNode.choices.length && <><p>The End</p><p><img src={import.meta.env.BASE_URL + "skull.gif"} alt={"human skull"} height={50} /></p></>}
   </>
 }
 
